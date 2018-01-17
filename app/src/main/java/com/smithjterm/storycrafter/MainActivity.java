0e6923e500e952f884f64c1227b035cdea59355f
Demo User <demouser@MacBook-Air.local>
2018-01-17 10:54:34 -0500

    First commit

---
+++ app/src/main/java/com/smithjterm/storycrafter/MainActivity.java
@@ -0,0 +1,96 @@
+package com.smithjterm.storycrafter;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // let's tell some sci-fi stories! :D - sarah
+    static final int EDIT_REQUEST = 1;
+    public static final String EDIT_ID_KEY = "edit id";
+    public static final String EDIT_TITLE_KEY = "edit title";
+    public static final String EDIT_BODY_KEY = "edit body";
+    public static final String EDIT_CHOICE_1_KEY = "edit choice 1";
+    public static final String EDIT_CHOICE_2_KEY = "edit choice 2";
+
+    StoryTree mainTree = new StoryTree(0);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mainTree.setLeft(new StoryTree(2));
+        mainTree.setRight(new StoryTree(1));
+
+        mainTree.getLeft().setLeft(new StoryTree(4));
+        mainTree.getLeft().setRight(new StoryTree(6));
+
+        mainTree.getRight().setLeft(new StoryTree(5));
+        mainTree.getRight().setRight(new StoryTree(3));
+
+        // The test below makes sure isFull works.
+
+        //  String result = "test result: "+mainTree.isFull();
+        //  Log.i("MainActivity",result);
+
+        // 2131165250 1 2 3 4 5 6
+
+        String test = " "+R.id.imageButton1+" "+R.id.imageButton2+" "+R.id.imageButton3+" "+R.id.imageButton4
+                +" "+R.id.imageButton5+" "+R.id.imageButton6+" "+R.id.imageButton7;
+
+        Log.i("MainActivity",test);
+
+        /*
+            Intent i = new Intent(this, SecondActivity.class);
+
+            i.putExtra(POST_KEY,postView.getText().toString());
+
+            startActivity(i);
+
+        } */
+    }
+
+    public void startNewActivity (View view){
+        int id = 2131165250 - view.getId();
+        StoryTree tweakNode = mainTree.treeSearch(id);
+
+        Intent intent = new Intent(this, NodeClicked.class);
+
+        //TextView postView = (TextView) findViewById(R.id.postEntry);
+        intent.putExtra(EDIT_ID_KEY,id);
+        intent.putExtra(EDIT_TITLE_KEY, tweakNode.getTitle());
+        intent.putExtra(EDIT_BODY_KEY, tweakNode.getBody());
+        intent.putExtra(EDIT_CHOICE_1_KEY, tweakNode.getChoice1Txt());
+        intent.putExtra(EDIT_CHOICE_2_KEY, tweakNode.getChoice2Txt());
+
+        startActivityForResult(intent, EDIT_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Check which request we're responding to
+        if (requestCode == EDIT_REQUEST) {
+            // Make sure the request was successful
+            if (resultCode == RESULT_OK) {
+                // The user EDITED THINGS.
+                // get EDITED THINGS.
+                Bundle extras = data.getExtras();
+                int id = 2131165250 + extras.getInt(EDIT_ID_KEY);
+                StoryTree tweakNode = mainTree.treeSearch(id);
+
+                tweakNode.setTitle(extras.getString(EDIT_TITLE_KEY));
+                tweakNode.setBody(extras.getString(EDIT_BODY_KEY));
+                tweakNode.setChoice1txt(extras.getString(EDIT_CHOICE_1_KEY));
+                tweakNode.setChoice1txt(extras.getString(EDIT_CHOICE_2_KEY));
+
+                // The Intent's data Uri identifies which contact was selected.
+                // Do something with the contact here (bigger example below)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/smithjterm/storycrafter/StoryTree.java
@@ -0,0 +1,125 @@
+package com.smithjterm.storycrafter;
+
+/**
+ * Created by Sarah Abowitz on 1/16/18.
+ */
+
+public class StoryTree {
+    private StoryTree left;
+    private StoryTree right;
+
+    private int id;
+    private String title;
+    private String body;
+    private String choice1txt;
+    private String choice2txt;
+
+    public StoryTree(){
+      title = "";
+      body = "";
+      choice1txt = "";
+      choice2txt = "";
+    }
+
+    public StoryTree(int x){
+        title = "";
+        body = "";
+        choice1txt = "";
+        choice2txt = "";
+        id = x;
+    }
+
+    public StoryTree(String t, String b, String c1, String c2, int x){
+      title = t;
+      body = b;
+      choice1txt = c1;
+      choice2txt = c2;
+      id = x;
+    }
+
+    public StoryTree getLeft() {
+        return left;
+    }
+
+    public StoryTree getRight() {
+        return right;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public String getChoice1Txt() {
+        return choice1txt;
+    }
+
+    public String getChoice2Txt() {
+        return choice2txt;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setLeft(StoryTree left) {
+        this.left = left;
+    }
+
+    public void setRight(StoryTree right) {
+        this.right = right;
+    }
+
+    public void setData(String t, String b, String c1, String c2){
+        title = t;
+        body = b;
+        choice1txt = c1;
+        choice2txt = c2;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public void setChoice1txt(String choice1txt) {
+        this.choice1txt = choice1txt;
+    }
+
+    public void setChoice2txt(String choice2txt) {
+        this.choice2txt = choice2txt;
+    }
+
+    public boolean isFull(){
+      if (title.length() < 1 || body.length() < 1 || choice1txt.length() < 1 || choice2txt.length() < 1){
+          return false;
+      }
+
+      if (left != null && right != null){
+          return (left.isFull() && right.isFull());
+      }
+
+      return true;
+    }
+
+    public StoryTree treeSearch(int num){
+        if (id == num) {
+            return this;
+        }
+
+        if (left != null && right != null){
+            StoryTree l = left.treeSearch(num);
+            StoryTree r = right.treeSearch(num);
+            if (l != null) return l;
+            if (r != null) return r;
+        }
+
+        return null;
+    }
+}
